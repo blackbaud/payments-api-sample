6290607e90a7b4787bba18552b9147ee6179be09
Ian Thomas <ian.thomas@blackbaud.com>
2024-06-05 12:24:12 -0400

    Set is_backoffice on create transaction request

--- server/dotnet-blazor/BusinessLogic/PaymentsService.cs
+++ server/dotnet-blazor/BusinessLogic/PaymentsService.cs
@@ -140,7 +140,8 @@ public class PaymentsService
             Amount = 1212,
             CardToken = cardToken,
             PaymentConfigurationId = paymentConfigurationId,
-            TransactionType = "CardNotPresent"
+            TransactionType = "CardNotPresent",
+            IsBackoffice = true
         };
 
         var requestBody = new StringContent(
