2b678cc3c0d0514b3e6be3bd4184db04b8fd7b77
Ian Thomas <ian.thomas@blackbaud.com>
2024-11-13 16:51:49 -0500

    Ensure local datastore directory exists before writing to directory (#8)
    * Ensure local datastore directory exists before writing to directory

--- server/dotnet-blazor/DataAccess/LocalFileDataAdapter.cs
+++ server/dotnet-blazor/DataAccess/LocalFileDataAdapter.cs
@@ -5,13 +5,16 @@ namespace Blackbaud.PaymentsAPITutorial.DataAccess;
 public class LocalFileDataAdapter
 {
     private readonly IHostEnvironment _env;
+    private const string DATA_STORE_DIRECTORY = "datastore";
 
     public LocalFileDataAdapter(IHostEnvironment env)
     {
         _env = env;
+        EnsureDirectoryExists();
     }
 
-    public async Task<T> ReadDataAsync<T>() where T : new()
+    public async Task<T> ReadDataAsync<T>()
+        where T : new()
     {
         var filePath = GetFilePath<T>();
         if (!File.Exists(filePath))
@@ -25,10 +28,7 @@ public class LocalFileDataAdapter
 
     public async Task WriteDataAsync<T>(T data)
     {
-        var options = new JsonSerializerOptions
-        {
-            WriteIndented = true
-        };
+        var options = new JsonSerializerOptions { WriteIndented = true };
 
         var json = JsonSerializer.Serialize(data, options);
         await File.WriteAllTextAsync(GetFilePath<T>(), json);
@@ -36,6 +36,15 @@ public class LocalFileDataAdapter
 
     private string GetFilePath<T>()
     {
-        return Path.Combine(_env.ContentRootPath, "datastore", typeof(T).Name + ".json");
+        return Path.Combine(_env.ContentRootPath, DATA_STORE_DIRECTORY, typeof(T).Name + ".json");
+    }
+
+    private void EnsureDirectoryExists()
+    {
+        var directory = Path.Combine(_env.ContentRootPath, DATA_STORE_DIRECTORY);
+        if (!Directory.Exists(directory))
+        {
+            Directory.CreateDirectory(directory);
+        }
     }
 }
