15c9b747482ac6aecc9651fe072b9ca8755fb18b
Ian Thomas <ian.thomas@blackbaud.com>
2026-03-23 14:02:32 -0400

    Integrate selecting of payment config into UI

--- client/admin/angular/src/app/home/home.ts
+++ client/admin/angular/src/app/home/home.ts
@@ -3,6 +3,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { AuthenticatedResponse } from '../shared/models/authenticated.response';
 import { PaymentConfigurationListRead } from '../shared/models/payment-configuration-list-read';
 import { PaymentConfigurationRead } from '../shared/models/payment-configuration-read';
+import { ProcessingConfiguration } from '../shared/models/processing-configuration';
 
 @Component({
   selector: 'app-home',
@@ -13,33 +14,49 @@ import { PaymentConfigurationRead } from '../shared/models/payment-configuration
 export class Home implements OnInit {
   public authenticated: boolean = false;
   public paymentConfigs: PaymentConfigurationRead[] = [];
+  public checkoutConfig?: ProcessingConfiguration;
+
+  #client = inject(HttpClient);
+
   public ngOnInit(): void {
-    this.httpClient
+    this.#client
       .get<AuthenticatedResponse>('https://localhost:5001/auth/authenticated')
       .subscribe((res: AuthenticatedResponse) => {
         this.authenticated = res.authenticated;
         if (this.authenticated) {
           this.fetchPaymentConfigs();
+          this.fetchCheckoutConfiguration();
         }
       });
   }
-  private httpClient = inject(HttpClient);
 
   public selectPaymentConfiguration(paymentConfigurationId: string): void {
-    this.httpClient
+    this.#client
       .post('https://localhost:5001/payments/paymentconfigurations/select', {
         payment_configuration_id: paymentConfigurationId,
       })
-      .subscribe(() => {});
+      .subscribe(() => {
+        this.fetchCheckoutConfiguration();
+      });
   }
 
   private fetchPaymentConfigs(): void {
-    this.httpClient
+    this.#client
       .get<PaymentConfigurationListRead>(
         'https://localhost:5001/payments/paymentconfigurations',
       )
       .subscribe((res: PaymentConfigurationListRead) => {
         this.paymentConfigs = res.value;
+      });
+  }
+
+  private fetchCheckoutConfiguration(): void {
+    this.#client
+      .get<ProcessingConfiguration>(
+        `https://localhost:5001/payments/checkoutconfiguration`,
+      )
+      .subscribe((configResponse: ProcessingConfiguration) => {
+        this.checkoutConfig = configResponse;
       });
   }
 }
