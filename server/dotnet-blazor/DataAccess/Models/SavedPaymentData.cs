fe21d19137e46ff72fa2ce500462e64f68716935
Ian Thomas <ian.thomas@blackbaud.com>
2026-04-08 12:58:46 -0400

    Remove dotnet-blazor example in favor of dotnet and new checkout

--- server/dotnet-blazor/DataAccess/Models/SavedPaymentData.cs
+++
@@ -1,7 +0,0 @@
-namespace Blackbaud.PaymentsAPITutorial.DataAccess.Models;
-
-public class SavedPaymentData
-{
-    public string CardToken { get; set; }
-    public string PaymentConfigurationId { get; set; }
-}
